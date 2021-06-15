63c4b41540d4dc01e7e0258bbc97c72a5209e0f0
Simon Mudd <smudd@amplience.com>
2021-06-15 08:59:35 +0100

    refactor(builds): changed the output dir and location of the source files

--- esbuild.js
+++ esbuild.js
@@ -1,9 +1,6 @@
 require('esbuild').build({
-  entryPoints: [
-    './typescript/main.js',
-    './typescript/services/http.service.ts',
-  ],
-  outdir: './esbuild-build',
+  entryPoints: ['./src/main.js', './src/services/http.service.ts'],
+  outdir: './builds/esbuild',
   minify: true,
   target: 'es2019',
   platform: 'node',
